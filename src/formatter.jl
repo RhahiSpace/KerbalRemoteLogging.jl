acd43a7978a124e870fe333558e39b1f8a118ca9
Rhahi <git@rhahi.com>
2023-05-29 23:47:08 +0200

    fix: update formatting

--- src/formatter.jl
+++ src/formatter.jl
@@ -11,20 +11,22 @@ function label(io::IO, msg)
 
     # Internals group / green
     msg.group == :eecom       ? printstyled(io, " [EECOM] "; color=2, bold=true) :
-    msg.group == :prop        ? printstyled(io, "  [PROP] "; color=2, bold=true) :
+    msg.group == :motor       ? printstyled(io, " [Motor] "; color=2, bold=true) :
     msg.group == :retro       ? printstyled(io, " [Retro] "; color=2, bold=true) :
-    msg.group == :power       ? printstyled(io, " [Power] "; color=2, bold=true) :
     msg.group == :system      ? printstyled(io, "[System] "; color=2, bold=true) :
     msg.group == :module      ? printstyled(io, "[Module] "; color=2, bold=true) :
+    msg.group == :stage       ? printstyled(io, " [Stage] "; color=2, bold=true) :
+    msg.group == :control     ? printstyled(io, "  [CTRL] "; color=22, bold=true) :
 
     # Programming group / red & magenta / grey
     msg.group == :develop     ? printstyled(io, "   [Dev] "; color=13, bold=true) :
+    msg.group == :index       ? printstyled(io, " [Index] "; color=5, bold=true) :
+    msg.group == :gui         ? printstyled(io, "   [GUI] "; color=8, bold=true) :
+    msg.group == :graphic     ? printstyled(io, " [Graph] "; color=8, bold=true) :
     msg.group == :entry       ? printstyled(io, " [Entry] "; color=8, bold=true) :
     msg.group == :exit        ? printstyled(io, "  [Exit] "; color=8, bold=true) :
-    msg.group == :controlloop ? printstyled(io, "[L-Ctrl] "; color=22, bold=true) :
-    msg.group == :gncloop     ? printstyled(io, " [L-GNC] "; color=17, bold=true) :
-    msg.group == :gui         ? printstyled(io, "   [GUI] "; color=5, bold=true) :
-    msg.group == :graphic     ? printstyled(io, " [Graph] "; color=5, bold=true) :
+    msg.group == :input       ? printstyled(io, " [Input] "; color=8, bold=true) :
+    msg.group == :event       ? printstyled(io, " [Event] "; color=8, bold=true) :
 
     # Science group / white
     # msg.group == :telemetry   ? printstyled(io, " [Telem]") :
@@ -34,7 +36,7 @@ function label(io::IO, msg)
     msg.group == :mark        ? printstyled(io, "  [Mark] "; color=11, bold=true) :
     # Misc
     msg.group == :time        ? printstyled(io, "  [Time] "; color=8,  bold=true) :
-    printstyled(io, lpad("[$(first(titlecase(string(msg.group)), 6))] ", 9); color=8, bold=true)
+    printstyled(io, lpad("[$(first(string(msg.group), 6))] ", 9); color=111, bold=true)
 end
 
 function label_status(io::IO, level::LogLevel)
@@ -45,16 +47,18 @@ function label_status(io::IO, level::LogLevel)
 end
 
 function ungrouped(io::IO, level::LogLevel)
-    level ≥ LogLevel(2000) ? printstyled(io, " [Error] "; color=1, bold=true) :
-    level ≥ LogLevel(1000) ? printstyled(io, "  [Warn] "; color=3, bold=true) :
-    level ≥ LogLevel(0)    ? printstyled(io, "  [Info] "; color=7, bold=true) :
-    printstyled(io, " [Debug] "; color=8, bold=true)
+    level ≥ Logging.Error  ? printstyled(io, " [Error] "; color=1, bold=true) :
+    level ≥ Logging.Warn   ? printstyled(io, "  [Warn] "; color=3, bold=true) :
+    level ≥ Logging.Info   ? printstyled(io, "  [Info] "; color=7, bold=true) :
+    level ≥ Logging.Debug  ? printstyled(io, " [Debug] "; color=8, bold=true) :
+    printstyled(io, " [Trace] "; color=235, bold=true)
 end
 
 function levelcolor(level::LogLevel)
-    level ≥ LogLevel(2000) ? 9 :
-    level ≥ LogLevel(1000) ? 3 :
-    level ≥ LogLevel(0) ? 7 : 8
+    level ≥ LogLevel(2000)  ? 9 :
+    level ≥ LogLevel(1000)  ? 3 :
+    level ≥ LogLevel(0)     ? 7 :
+    level ≥ LogLevel(-1000) ? 8 : 235
 end
 
 function kerbal_formatter(timestring::Function)
@@ -63,10 +67,9 @@ function kerbal_formatter(timestring::Function)
             body = levelcolor(args.level)
             printstyled(io, timestring(), " "; color=body)
             label(io, args)
-            kwargs = args.kwargs |> values
-            if length(kwargs) > 0
+            if length(args.kwargs) > 0
                 printstyled(io, args.message, "\n"; color=body)
-                for (k, v) ∈ kwargs
+                for (k, v) ∈ args.kwargs
                     printstyled(io, "  $k = $v\n"; color=body)
                 end
             else
